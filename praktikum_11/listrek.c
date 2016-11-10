997f7c421360af29d20095e23f83000e1fd0173c
Jauhar Arifin <jauhararifin10@gmail.com>
2016-11-10 11:37:33 +0700

    praktikum 11 selesai

--- praktikum_11/listrek.c
+++ praktikum_11/listrek.c
@@ -193,3 +193,146 @@ boolean Search (List L, infotype X) {
 	else
 		return Search(Tail(L), X);
 }
+
+/* 
+ * Inverse
+ * Mengirimkan list baru, hasil invers dari L dengan menyalin semua elemen list.
+   Semua elemen list baru harus dialokasi
+ * Jika alokasi gagal, hasilnya list kosong
+ */
+List Inverse (List L) {
+	if (L == Nil)
+		return Nil;
+	else
+		return KonsB(Inverse(Tail(L)), FirstElmt(L));
+}
+
+/* 
+ * SplitPosNeg
+ * I.S. L mungkin kosong
+ * F.S. Berdasarkan L, dibentuk dua buah list L1 dan L2
+ * L1 berisi semua elemen L yang positif atau 0, sedangkan L2 berisi
+   semua elemen L yang negatif; semua dengan urutan yang sama seperti di L
+ * L tidak berubah: Semua elemen L1 dan L2 harus dialokasi
+ * Jika L kosong, maka L1 dan L2 kosong
+ */
+void SplitPosNeg (List L, List *L1, List *L2) {
+	if (L != Nil) {
+		if (FirstElmt(L) >= 0)
+			*L1 = KonsB(*L1, FirstElmt(L));
+		else
+			*L2 = KonsB(*L2, FirstElmt(L));
+		SplitPosNeg(Tail(L), L1, L2);
+	}
+}
+
+/* 
+ * SplitOnX
+ * I.S. L dan X terdefinisi, L1 dan L2 sembarang.
+ * F.S. L1 berisi semua elemen L yang lebih kecil dari X, dengan urutan
+   kemunculan yang sama, L2 berisi semua elemen L yang tidak masuk ke
+   L1, dengan urutan kemunculan yang sama.
+ */
+void SplitOnX (List L, infotype X, List *L1, List *L2) {
+	if (L != Nil) {
+		if (FirstElmt(L) < X)
+			*L1 = KonsB(*L1, FirstElmt(L));
+		else
+			*L2 = KonsB(*L2, FirstElmt(L));
+		SplitOnX(Tail(L), X, L1, L2);
+	}
+}
+
+/* 
+ * ListCompare
+ * Menghasilkan: -1 jika L1 < L2, 0 jika L1 = L2, dan 1 jika L1 > L2
+ * Jika L[i] adalah elemen L pada urutan ke-i dan |L| adalah panjang L:
+ * L1 = L2: |L1| = |L2| dan untuk semua i, L1[i] = L2[i]
+ * L1 < L2: Jika i adalah urutan elemen yang berbeda yang terkecil
+   dari L1 dan L2, L1[i] < L2[i] atau: Jika pada semua elemen pada
+   urutan i yang sama, L1[i]=L2[i], namun |L1| < |L2|
+ * Contoh: [3,5,6,7] < [4,4,5,6]; [1,2,3] < [1,2,3,4]
+ * L1 > L2: kebalikan dari L1 < L2
+ */
+int ListCompare (List L1, List L2) {
+	if (L1 == Nil && L2 == Nil)
+		return 0;
+	else if (L1 == Nil)
+		return -1;
+	else if (L2 == Nil)
+		return 1;
+	else if (FirstElmt(L1) < FirstElmt(L2))
+		return -1;
+	else if (FirstElmt(L1) == FirstElmt(L2))
+		return ListCompare(Tail(L1), Tail(L2));
+	else
+		return 1;
+}
+
+/* 
+ * IsAllExist
+ * Menghasilkan true jika semua elemen dalam L1 terdapat dalam L2
+ * (tanpa memperhatikan urutan ataupun banyaknya elemen).
+ * Kedua list mungkin kosong. Jika L1 kosong, maka hasilnya false.
+ */
+boolean IsAllExist (List L1, List L2) {
+	if (L1 == Nil)
+		return true;
+	else
+		return Search(L2, FirstElmt(L1)) && IsAllExist(Tail(L1), L2);
+}
+
+/* 
+ * Max
+ * Mengirimkan nilai info(P) yang maksimum
+ * Prekondisi untuk Max/Min/Sum/Average : List tidak kosong
+ */
+infotype Max (List L) {
+	if (IsOneElmt(L))
+		return FirstElmt(L);
+	else {
+		infotype x = Max(Tail(L));
+		if (x > FirstElmt(L))
+			return x;
+		else
+			return FirstElmt(L);
+	}
+}
+
+/* 
+ * Min
+ * Mengirimkan nilai info(P) yang minimum
+ * Prekondisi untuk Max/Min/Sum/Average : List tidak kosong
+ */
+infotype Min (List L) {
+	if (IsOneElmt(L))
+		return FirstElmt(L);
+	else {
+		infotype x = Min(Tail(L));
+		if (x < FirstElmt(L))
+			return x;
+		else
+			return FirstElmt(L);
+	}
+}
+
+/* 
+ * SumList
+ * Mengirimkan total jumlah elemen List L
+ * Prekondisi untuk Max/Min/Sum/Average : List tidak kosong
+ */
+infotype SumList (List L) {
+	if (L == Nil)
+		return 0;
+	else
+		return FirstElmt(L) + SumList(Tail(L));
+}
+
+/* 
+ * Average
+ * Mengirimkan nilai rata-rata elemen list L
+ * Prekondisi untuk Max/Min/Sum/Average : List tidak kosong
+ */
+float Average (List L) {
+	return (float) SumList(L)/ (float) NbElmtList(L);
+}
